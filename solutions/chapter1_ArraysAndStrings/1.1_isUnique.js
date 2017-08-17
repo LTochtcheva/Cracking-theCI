ab47d0c9d2071bff20dded3ac408a56916233dae
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-17 14:31:19 -0400

    add solution 1.1 no additional data structures

--- solutions/chapter1_ArraysAndStrings/1.1_isUnique.js
+++ solutions/chapter1_ArraysAndStrings/1.1_isUnique.js
@@ -1,13 +1,21 @@
 //Implement an algorithm to determine if a string has all unique
 //characters.
 //What if you can't use additional data structures?
-export default  (str) => {
-  console.log('func is running' );
+export const funcSet = (str) => {
+
   const set = new Set();
 
   for (let i = 0; i < str.length; i++) {
     if (set.has(str[i])) return false;
     else set.add(str[i]);
+  }
+  return true;
+}
+
+export const funcSort = (str) => {
+  let sortedStr = str.split('').sort();
+  for (let i = 0; i < sortedStr.length; i++) {
+    if (sortedStr[i] === sortedStr[i+1]) return false;
   }
   return true;
 }
