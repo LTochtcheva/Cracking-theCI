ed832773d1f7667400cff6ac1309719c0a2c0075
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-19 12:01:45 -0400

    add 1.6 test

---
+++ solutions/chapter1_ArraysAndStrings/1.6_stringCompression.js
@@ -0,0 +1,8 @@
+// Implement a method to perform basic string compression using the counts
+// of repeated chars.
+// "aabcccccaaa" -> "a2b1c5a3"
+// if compressed is not shorter than original, return original string
+
+export default (str) => {
+  console.log(str)
+}
---
+++ test/1.6_stringCompressionTest.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import func from '../solutions/chapter1_ArraysAndStrings/1.6_stringCompression.js';
+
+describe('method to compress a string with counts:\n', () => {
+
+  it('should return correctly compressed string', () => {
+    expect(func('aasdddfgggg')).to.equal('a2s1d3f1g4');
+    expect(func('aaaaa')).to.equal('a5');
+    expect(func('')).to.equal('');
+    expect(func(null)).to.equal(null);
+    expect(func(undefined)).to.equal(undefined);
+  });
+
+  it('should return original string if compressed string is not shorter', () => {
+    expect(func('abcd')).to.equal('abcd');
+    expect(func('aacd')).to.equal('aacd');
+  });
+
+});
+
