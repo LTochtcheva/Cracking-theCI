6690b6e11e51630eb1dddf23e3d31d8d441859fd
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-18 22:35:25 -0400

    add 1.5 test

---
+++ solutions/chapter1_ArraysAndStrings/1.5_OneAway.js
@@ -0,0 +1,14 @@
+// There are 3 types of edits for strings:
+// insert a char
+// remove a char
+// replace a char
+// Given two strings check if they are <= one edit away
+// pale, ple -> true, 'a' removed
+// pale, bale -> true, 'p' replaced with 'b'
+// pale, pale -> true, 0 edits
+// pale, bake -> false, 'p'-'b' and 'l'-'k', 2 edits
+
+export default (str, edited) => {
+
+  console.log('running')
+}
---
+++ test/1.5_OneAwayTest.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+import func from '../solutions/chapter1_ArraysAndStrings/1.5_OneAway.js';
+
+describe('method to check if two strings are one edit away:\n', () => {
+
+  it('should return true if strings are one or zero edits away', () => {
+    expect(func('cat', 'cats')).to.equal(true);
+    expect(func('cat', 'cat')).to.equal(true);
+    expect(func('asdfghjkl12345', 'asdfghkl12345')).to.equal(true);
+    expect(func('qwerty', 'Zwerty')).to.equal(true);
+    expect(func('', 'a')).to.equal(true);
+  });
+
+  it('should return false if strings are more than one edit away', () => {
+    expect(func('Hello World', 'Hello World!!!')).to.equal(false);
+    expect(func('Hi', 'Buy')).to.equal(false);
+    expect(func('Labrador', 'Lab')).to.equal(false);
+  });
+
+  it('should handle falsy string values correctly', () => {
+    expect(func('', '')).to.equal(true);
+    expect(func(null, null)).to.equal(true);
+    expect(func(undefined, undefined)).to.equal(true);
+    expect(func('', null)).to.equal(false);
+    expect(func(undefined, '')).to.equal(false);
+    expect(func(null, undefined)).to.equal(false);
+  });
+
+});
