3db24b7b2bef0a7b36479f1d3a684683d9887074
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-18 22:14:31 -0400

    add 1.4 solution

--- solutions/chapter1_ArraysAndStrings/1.4_PalindromePermutation.js
+++ solutions/chapter1_ArraysAndStrings/1.4_PalindromePermutation.js
@@ -1,1 +1,20 @@
+// Given a string write a method to check if it is a
+// permutation of palindrome
+// Palindrome - word or phrase that is the same forwards and backwards.
+// Permutation - rearrangement of letters
+// input "Tact Coa"
+// output True, permutations: "taco cat", "atco cta", ...
 
+export default (str) => {
+
+  if (!str) return false;
+
+  let string = str.toLowerCase().split(' ').join('');
+  let lettersCount = new Set();
+
+  for (let i = 0; i < string.length; i++) {
+    if (lettersCount.has(string[i])) lettersCount.delete(string[i]);
+    else lettersCount.add(string[i]);
+  }
+  return lettersCount.size <= 1;
+}
