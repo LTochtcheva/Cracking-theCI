41d1c24e7263011a5d68cedebb3bec3c5a1d73a5
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-18 20:46:12 -0400

    add 1.3. solution

--- solutions/chapter1_ArraysAndStrings/1.3_URLify.js
+++ solutions/chapter1_ArraysAndStrings/1.3_URLify.js
@@ -1,1 +1,7 @@
 // Write a method to replace all  spaces in a string with '%20'
+
+export default (str) => {
+
+  let noSpaceArr = str.split(' ').filter((word) => word !== '');
+  return noSpaceArr.join('%20');
+}
--- test/1.3_URLifyTest.js
+++ test/1.3_URLifyTest.js
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import func from '../solutions/chapter1_ArraysAndStrings/1.3_URLify.js';
 
 
-describe(':\n', () => {
+describe('method to replace spaces in a string with "%20":\n', () => {
 
-  it('should', () => {
+  it('should return correct string', () => {
     expect(func('Hello World')).to.equal('Hello%20World');
     expect(func('Hello  great   developer  ')).to.equal('Hello%20great%20developer');
     expect(func('Hi')).to.equal('Hi');
