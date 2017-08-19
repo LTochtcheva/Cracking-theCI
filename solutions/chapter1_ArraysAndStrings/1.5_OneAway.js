15fc23c1a59c82f2897497ede9730483117d96d2
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-19 11:43:26 -0400

    1.5 solution

--- solutions/chapter1_ArraysAndStrings/1.5_OneAway.js
+++ solutions/chapter1_ArraysAndStrings/1.5_OneAway.js
@@ -8,7 +8,28 @@
 // pale, pale -> true, 0 edits
 // pale, bake -> false, 'p'-'b' and 'l'-'k', 2 edits
 
-export default (str, edited) => {
+export default (str1, str2) => {
 
-  console.log('running')
+  if (Math.abs(str1.length - str2.length) > 1) return false;
+
+  let inserted, removed;
+  let oneEdit = false;
+
+  if (str1.length < str2.length) {
+    inserted = true;
+  }
+  if (str1.length > str2.length) {
+    removed = true;
+  }
+
+  for (let i = 0, j = 0; i < str1.length && j < str2.length; i++, j++) {
+
+    if (str1[i] !== str2[j]) {
+      if(oneEdit) return false;
+      if (inserted) i --;
+      if (removed)  j --;
+      oneEdit = true;
+    }
+  }
+  return true;
 }
