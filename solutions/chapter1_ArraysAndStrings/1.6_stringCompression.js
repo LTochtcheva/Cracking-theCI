4b07e4c34b31e1bcdf2b82d63bbaddb5386e6683
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-19 12:30:57 -0400

    add 1.6 solution

--- solutions/chapter1_ArraysAndStrings/1.6_stringCompression.js
+++ solutions/chapter1_ArraysAndStrings/1.6_stringCompression.js
@@ -4,5 +4,21 @@
 // if compressed is not shorter than original, return original string
 
 export default (str) => {
-  console.log(str)
+
+  if (!str) return str;
+
+  let count = 1;
+  let result = '';
+
+  for (let i = 0; i < str.length; i ++) {
+
+    if (str[i] === str[i + 1]) {
+      count ++;
+    }
+    else {
+      result = result + str[i] + count.toString();
+      count = 1;
+    }
+  }
+  return result.length < str.length ? result : str;
 }
