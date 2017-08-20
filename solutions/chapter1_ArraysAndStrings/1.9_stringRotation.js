f149f018f46cd5fa4551d71214335faa00f65aeb
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-19 21:07:10 -0400

    add 1.9 solution

--- solutions/chapter1_ArraysAndStrings/1.9_stringRotation.js
+++ solutions/chapter1_ArraysAndStrings/1.9_stringRotation.js
@@ -1,1 +1,9 @@
-1.9_stringRotation.js
+// Given str1 and str2 check if str2 is a rotation of str1, using one call
+// to isSubstring
+// waterbottle is rotation of erbottlewat -> true
+
+export default (str1, str2) => {
+  let doubleStr1 = str1 + str1;
+  if (str1.length !== str2.length) return false;
+  else return doubleStr1.includes(str2);
+}
--- test/1.9_stringRotationTest.js
+++ test/1.9_stringRotationTest.js
@@ -4,7 +4,7 @@ import func from '../solutions/chapter1_ArraysAndStrings/1.9_stringRotation.js';
 describe('method to check if str2 is a rotation of str1 using one call to isSubstring:\n', () => {
 
   it('should return true if str2 is a rotation of str1', () => {
-    expect(func('asdfg', 'dfgas')).to.equal('true');
+    expect(func('asdfg', 'dfgas')).to.equal(true);
     expect(func('erbottlewat', 'waterbottle')).to.equal(true);
     expect(func('catdog', 'dogcat')).to.equal(true);
   });
