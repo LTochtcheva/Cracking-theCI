5c2f31e28cf49938bf54725a4fccd3a97e3a109e
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-21 10:18:49 -0400

    add 1.7 solution

--- solutions/chapter1_ArraysAndStrings/1.7_rotateMatrix.js
+++ solutions/chapter1_ArraysAndStrings/1.7_rotateMatrix.js
@@ -2,4 +2,14 @@
 
 export default (matrix) => {
 
+  let result = [];
+
+  for (let j = 0; j < matrix.length; j ++) {
+    let row = [];
+    for (let i = matrix.length -1; i >= 0; i --) {
+      row.push(matrix[i][j]);
+    }
+    result[j] = row;
+  }
+  return result;
 }
