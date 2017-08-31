040cb5687a2f4d1d9e6851736e2541f57ec11c9e
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-31 14:14:00 -0400

    add 2.2 solution with two pointers

--- solutions/chapter2_LinkedLists/2.2_ReturnKthToLast.js
+++ solutions/chapter2_LinkedLists/2.2_ReturnKthToLast.js
@@ -5,13 +5,33 @@
 //[1,2,3,4,5,6],3 -> 3
 //return null if list is not long enough
 
+// export default (list, k) => {
+//   const places = {}
+//   let pointer = 0
+//   while (list) {
+//     places[pointer] = list.val
+//     pointer ++
+//     list = list.next
+//   }
+//   return places[pointer - 1 - k] || null
+// }
+
+// solution with two pointers
+
 export default (list, k) => {
-  const places = {}
-  let pointer = 0
-  while (list) {
-    places[pointer] = list.val
-    pointer ++
-    list = list.next
+  let current = list
+  let ahead = list
+  let count = 0
+
+  while (count <= k) {
+    if (!ahead) return null
+    ahead = ahead.next
+    count++
   }
-  return places[pointer - 1 - k] || null
+  while (ahead) {
+    ahead = ahead.next
+    current = current.next
+  }
+
+  return current.val
 }
