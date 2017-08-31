7c946a71a9fe383a79b1fdae05b3a2495f9ecf1f
Lada Tochtcheva <lada.toch@gmail.com>
2017-08-31 13:35:42 -0400

    add 2.2 test

---
+++ solutions/chapter2_LinkedLists/2.2_ReturnKthToLast.js
@@ -0,0 +1,10 @@
+// Implement an algorithm to find Kth to last
+//element of a singly linked list
+// 0th will return last element
+//[1,2,3,4,5,6],0 -> 6
+//[1,2,3,4,5,6],3 -> 3
+//return null if list is not long enough
+
+export default (list, k) => {
+
+}
---
+++ test/Chapter2/2.2_ReturnKthToLastTest.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import * as utils from './utilsChapter2'
+import func  from '../../solutions/chapter2_LinkedLists/2.2_ReturnKthToLast'
+
+describe('method to find kth to last element of linked list:\n', () => {
+
+  let testData = [
+    {input: [1, 2, 3],
+     k: 0,
+     result: 3},
+    {input: [1],
+     k: 2,
+     result: null},
+    {input: [1, 2, 3, 4, 5, 6, 7, 8],
+     k: 4,
+     result: 4},
+    {input: [1, 2, 3, 3, 4, 5, 4, 5],
+     k: 3,
+     result: 4},
+  ]
+
+  testData.forEach(data => {
+    it(`returns ${data.k}th to the last element of ${data.input}`, () => {
+      let list = utils.arrayToLinkedList(data.input)
+      expect(func(list)).to.eql(data.result)
+    })
+  })
+
+})
+
