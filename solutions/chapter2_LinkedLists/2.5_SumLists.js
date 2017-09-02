0848d186560022c6497a32eaab2ed57b91fe20fa
Lada Tochtcheva <lada.toch@gmail.com>
2017-09-02 17:31:29 -0400

    add 2.5 test

---
+++ solutions/chapter2_LinkedLists/2.5_SumLists.js
@@ -0,0 +1,13 @@
+//1)Sum two numbers, represented by a linked list in
+//reverse order
+//7-1-6 + 5-9-2 = 617+295 = 912 -> 2-1-9
+
+export const sumReversed = (list1, list2) => {
+
+return true
+}
+
+export const sumForward = (list1, list2) => {
+
+  return true
+}
---
+++ test/Chapter2/2.5_SumListsTest.js
@@ -0,0 +1,44 @@
+import { expect } from 'chai'
+import * as utils from './utilsChapter2'
+import {sumReversed, sumForward} from '../../solutions/chapter2_LinkedLists/2.5_SumLists'
+
+describe('Sum two numbers represented by a linked list:\n', () => {
+
+    let testData = [
+    {list1: [1, 2],
+     list2: [3, 4],
+     result1: [4, 6],
+     result2: [4, 6]},
+    {list1: [1, 2, 1, 3],
+     list2: [3, 4],
+     result1: [4, 6, 1, 3],
+     result2: [7, 4, 2, 1]},
+    {list1: [1, 2],
+     list2: [3, 4, 5],
+     result1: [4, 6, 5],
+     result2: [7, 5, 3]},
+    {list1: [1, 2],
+     list2: [9, 8],
+     result1: [0, 1, 1],
+     result2: [0, 1, 1]}
+  ]
+
+    testData.forEach(data => {
+      it(`should return a sum of nums represented by reversed linked list:  ${data.list1}, ${data.list2} `, () => {
+        let list1 = utils.arrayToLinkedList(data.list1)
+        let list2 = utils.arrayToLinkedList(data.list2)
+        let sum = sumReversed(list1, list2)
+        expect(utils.linkedListToArray(sum)).to.eql(data.result1)
+      })
+    })
+
+    testData.forEach(data => {
+      it(`should return a sum of nums represented by direct linked list:  ${data.list1}, ${data.list2} `, () => {
+        let list1 = utils.arrayToLinkedList(data.list1)
+        let list2 = utils.arrayToLinkedList(data.list2)
+        let sum = sumReversed(list1, list2)
+        expect(utils.linkedListToArray(sum)).to.eql(data.result2)
+      })
+    })
+
+})
