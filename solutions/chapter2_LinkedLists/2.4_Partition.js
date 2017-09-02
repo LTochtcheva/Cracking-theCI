bca63ae33e4b530e95f04e56e3ebc3784472a4c6
Lada Tochtcheva <lada.toch@gmail.com>
2017-09-02 14:54:47 -0400

    add 2.4 solution

--- solutions/chapter2_LinkedLists/2.4_Partition.js
+++ solutions/chapter2_LinkedLists/2.4_Partition.js
@@ -1,1 +1,43 @@
-2.4_Partition.js
+//Partition linked current around given value, such that
+//all nodes less than value come before all nodes
+//greater than or equal to value
+//1, 9, 3, 2, 8, 4, 5 val = 3
+//1
+//1     9
+//1     9,3
+//1,2   9,3
+//1,2   9,3,8
+//1,2   9,3,8,4
+//1,2   9,3,8,4,5
+//1,2,9,3,8,4,5
+
+export default (list, val) => {
+  let smaller, greater, smallerHead, greaterHead
+  let current = list
+  while (current) {
+    if (current.val < val) {
+      if (smaller)
+        {smaller.next = current
+         smaller = smaller.next
+         }
+      else { smaller = current
+      smallerHead = current
+      }
+    }
+    else  {
+      if (greater)
+        {greater.next = current
+          greater = greater.next
+        }
+      else {
+        greater = current
+        greaterHead = current
+      }
+    }
+  current = current.next
+}
+
+  if (!smaller || !greater) return list
+  smaller.next = greaterHead
+  return smallerHead
+}
